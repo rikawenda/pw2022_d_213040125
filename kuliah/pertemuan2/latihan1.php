eb3f62741f9293c681dc1a12d214ed8447b77ef8
rikawenda <febriyantirika081@gmail.com>
2022-03-02 11:10:56 +0700

    menyelesaikan tugas pertemuan2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,60 @@
+<?php
+// pertemuan 2, belajarb mengenai sintaks php
+// echo untuk mencetak nilai ke layar
+// ctrl slash untuk menambahkan komentar
+echo 5 +5;
+
+echo "<hr>";
+
+//operator
+// aritmatika
+// +, -, *,/, %
+
+echo "<hr>";
+echo 7 % 3; // sisa bagi
+
+//perbandingaan
+// <, >, <=, >=, ==, !=
+echo "<hr>";
+echo 10 > 2;
+
+//variabel 
+// tidak boleh mengandung spasi
+// huruf pertama tidak boleh angka
+// snake_case : $nama_depan_mahasiswa
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+echo "<hr>";
+$nama = 'Rika Febriyanti';
+echo $nama;
+echo "<hr>"
+$x = 5;
+$y = 3;
+$z = $x + $y;
+echo $z;
+echo "<hr>";
+
+// penugasan/assigment
+// =, +=, -=, *=, /=, %=
+echo "<hr>";
+$aa =15;
+$aa +=5;
+$aa +=2;
+echo $a;
+
+//increment & decrement
+// ++$, --$, $++, $--
+echo "<hr>";
+$b = 10;
+echo $b--;
+echo "<hr>";
+echo --$b;
+echo "<hr>";
+echo $b;
+
+// identitas 
+// ===
+echo "<hr>";
+echo 10 ==="10";
+
+?>
